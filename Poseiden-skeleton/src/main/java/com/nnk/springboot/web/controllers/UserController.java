5e539fc9d8f7c2744944160e9e2e60d043eda301
Lyline <greg.quesne@gmail.com>
2022-02-14 15:00:06 +0100

    Refactor - Replace the repository by the UserService in user web controller

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/web/controllers/UserController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/web/controllers/UserController.java
@@ -1,8 +1,7 @@
 package com.nnk.springboot.web.controllers;
 
 import com.nnk.springboot.domain.User;
-import com.nnk.springboot.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.nnk.springboot.service.UserServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,16 +11,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
+
+    private final UserServiceImpl service;
+
+    public UserController(UserServiceImpl service) {
+        this.service = service;
+    }
 
     @RequestMapping("/user/list")
     public String home(Model model)
     {
-        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("users", service.getAll());
         return "user/list";
     }
 
@@ -35,8 +39,8 @@ public class UserController {
         if (!result.hasErrors()) {
            /* BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
             user.setPassword(encoder.encode(user.getPassword()));*/
-            userRepository.save(user);
-            model.addAttribute("users", userRepository.findAll());
+            service.create(user);
+            model.addAttribute("users", service.getAll());
             return "redirect:/user/list";
         }
         return "user/add";
@@ -44,7 +48,7 @@ public class UserController {
 
     @GetMapping("/user/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        User user = service.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
         user.setPassword("");
         model.addAttribute("user", user);
         return "user/update";
@@ -59,17 +63,21 @@ public class UserController {
 
         /*BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         user.setPassword(encoder.encode(user.getPassword()));*/
-        user.setId(id);
-        userRepository.save(user);
-        model.addAttribute("users", userRepository.findAll());
+
+        service.update(id,user);
+        model.addAttribute("users", service.getAll());
         return "redirect:/user/list";
     }
 
     @GetMapping("/user/delete/{id}")
     public String deleteUser(@PathVariable("id") Integer id, Model model) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        userRepository.delete(user);
-        model.addAttribute("users", userRepository.findAll());
+        Optional<User> user = service.findById(id);
+
+        if (!user.isEmpty()) {
+            service.delete(id);
+        }else new IllegalArgumentException("Invalid user Id:" + id);
+
+        model.addAttribute("users", service.getAll());
         return "redirect:/user/list";
     }
 }
